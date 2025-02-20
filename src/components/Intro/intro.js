52ccd81bd0d64f7917c0491b6750b6249361dacb
nagarajan-tr <trnagaram2003@gmail.com>
2025-02-20 10:55:33 +0530

    Updated features or bug fix description

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -4,7 +4,7 @@ import bg from '../../assets/image2.png';
 import { Link } from 'react-scroll';
 import btnImg from '../../assets/hireme.png';
 import resume from '../../assets/resume.png';
-import Nagarajan_resume from '../../assets/Nagarajan_resume.pdf';
+import Nagarajan_resume from '../../assets/nagarajan.pdf';
 
 const Intro = () => {
   return (
